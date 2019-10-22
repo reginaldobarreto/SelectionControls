28c0947ebf7c4461272a47083fb0974d0ff8b9ac
Reginaldo Barreto <reginaldo.barreto@gmail.com>
2019-10-21 21:28:01 -0300

    adicionado funcionalidade ao componente switch

--- app/src/main/java/br/com/barrsoft/selectioncontrols/MainActivity.java
+++ app/src/main/java/br/com/barrsoft/selectioncontrols/MainActivity.java
@@ -1,14 +1,40 @@
 package br.com.barrsoft.selectioncontrols;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.switchControl)
+    SwitchCompat switchCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    Toast.makeText(getApplicationContext(),R.string.checkOn,Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(getApplicationContext(),R.string.checkOff,Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
